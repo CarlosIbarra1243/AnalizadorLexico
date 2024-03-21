cd386bfd6aa9c2889056a54ba810630e7e7609b8
CarlosIbarra1243 <148893920+CarlosIbarra1243@users.noreply.github.com>
2024-03-21 07:53:25 -0700

    correccion en deteccion de cadenas

--- src/main/java/com/example/analizadorlexico/HelloController.java
+++ src/main/java/com/example/analizadorlexico/HelloController.java
@@ -35,13 +35,15 @@ public class HelloController implements Initializable {
     @FXML
     private TextArea text1;
 
-
     String lineaG = "";
+    String cadenaG ="";
+
+    String orCardena ="";
     //lista palabra reservadas
     ObservableList<String> palabrasReservadas = FXCollections.observableArrayList("byte", "short", "int","long","char","float","double","boolean","void","if","else","switch","case","default"
             ,"while","do","for","break","continue","try","catch","finally","throw","throws","private","protected","public","class","interface","enum","import","Main","main","compile","run","package","extends","implements","static","final","abstract","default","new","instanceof","this","super","return","var","synchronized","volatile","native","string","transient","assert","strictfp","Compile","ObservableList","Observable","ArrayList","ResourceBundle","Pattern","FXML","PropertyValueFactory","URL","FXMLLoader","Scene","Stage","IOException","HelloApplication","Application","launch","HelloController","Initializable","Button","TableColumn","TableView","TextArea","TextField","Label","CheckBox","RadioButton","ComboBox","DatePicker","ProgressBar","ProgressIndicator","Slider","Spinner","SplitPane","ScrollPane","Pagination","Pagination","Accordion","TitledPane","TabPane","ToolBar","MenuBar","Menu","MenuItem","ContextMenu","ContextMenuEvent","Dialog","Alert","FileChooser","DirectoryChooser","FileChooser"
             ,"package","extends","implements","static","final","abstract","default","new","instanceof","this","super","return","var","synchronized","volatile","native","string","transient","assert","strictfp","Out","PrintStream","InputStream","OutputStream","Reader","Writer","File","FileReader","FileWriter","BufferedReader","BufferedWriter","PrintWriter","InputStreamReader","OutputStreamWriter","Print","print","Println","println"
-            ,"const","goto","true","false","null","String","System","out","println","print","Scanner","nextInt","nextLine","nextDouble","nextBoolean","nextFloat","nextLong","nextShort","nextByte","next", "getItems","setItems", "getSelectedItem", "setSelectedItem","At","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","Matches","match","matches","Match");
+            ,"const","goto","true","toLowerCase","in","length","false","null","String","System","out","println","print","Scanner","nextInt","nextLine","nextDouble","nextBoolean","nextFloat","nextLong","nextShort","nextByte","next", "getItems","setItems", "getSelectedItem", "setSelectedItem","At","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","at","add","addAll","clear","contains","get","indexOf","isEmpty","remove","removeAll","retainAll","set","size","sort","subList","toArray","toString","equals","hashCode","notify","notifyAll","wait","clone","finalize","getClass","registerNatives","Matches","match","matches","Match");
 
     //lista de simbolos
     ObservableList<String> simbolos = FXCollections.observableArrayList(
@@ -79,8 +81,13 @@ public class HelloController implements Initializable {
             tbl1.getItems().add(new Datos(cadena, "Simbolo", columna, fila));
         } else if (variable.matcher(cadena).matches()) {
             int indice = lineaG.indexOf(cadena);
-            if (lineaG.charAt(indice-1) == '\"' && lineaG.charAt(indice+cadena.length()) == '\"'){
-                tbl1.getItems().add(new Datos(cadena, "cadena", columna, fila));
+            if (lineaG.charAt(indice-1) == '\"'){
+                Pattern pattern = Pattern.compile("\"([^\"]*)\"");
+                Matcher matcher = pattern.matcher(lineaG);
+                if(matcher.find()){
+                    cadenaG = matcher.group(1);
+                    tbl1.getItems().add(new Datos(cadenaG, "cadena", columna, fila));
+                }
             }else tbl1.getItems().add(new Datos(cadena, "variable", columna, fila));
 
         } else if (numero.matcher(cadena).matches()) {
@@ -90,8 +97,11 @@ public class HelloController implements Initializable {
         }
     }
 
+
     @FXML
     protected void calcular() {
+        lineaG = "";
+        cadenaG = "";
         // Limpiar tabla
         tbl1.getItems().clear();
         // Iniciar contador (para las filas)
@@ -103,6 +113,7 @@ public class HelloController implements Initializable {
         // Recorrer cada línea
         for (String linea : lineas) {
             lineaG = linea;
+            cadenaG = "";
             // Dividir la cadena teniendo en cuenta los límites de palabras no alfanuméricas
             String[] elementos = linea.split("(?<=\\W)|(?=\\W)");
             int contadorColumna = 1;
@@ -111,8 +122,8 @@ public class HelloController implements Initializable {
             for (String elemento : elementos) {
                 // Evitar procesar espacios en blanco vacíos
                 if (!elemento.trim().isEmpty()) {
-                    System.out.println("Fila: " + contadorFila + ", Columna: " + contadorColumna + ", Elemento: " + elemento);
-                    definirTipo(elemento, contadorFila, contadorColumna);
+                    // System.out.println("Fila: " + contadorFila + ", Columna: " + contadorColumna + ", Elemento: " + elemento);
+                    if (!cadenaG.contains(elemento))definirTipo(elemento, contadorFila, contadorColumna);
                 }
                 contadorColumna++;
             }
@@ -120,7 +131,8 @@ public class HelloController implements Initializable {
         }
     }
 
-    //limpiar tabla y textArea
+
+            //limpiar tabla y textArea
     @FXML
     protected void limpiar(){
         tbl1.getItems().clear();
